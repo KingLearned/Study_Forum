74ab792b00404e47dcbc87cbd06ee3f227331a28
kinglearned <kinglearned@gmail.com>
2024-01-11 15:42:23 +0100

    Made some changes in the login template

--- base/models.py
+++ base/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    name = models.CharField(max_length=200, null=True)
+    name = models.CharField(max_length=250, null=True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
 
