b3f9093f9580954c302f99b09d916cbaf4991db2
kinglearned <kinglearned@gmail.com>
2024-01-16 13:50:31 +0100

    paginated the profile section

--- config/settings.py
+++ config/settings.py
@@ -130,12 +130,13 @@ DEBUG = False
 STATIC_URL = '/static/'
 
 STATIC_ROOT = '/home/Mrlearned/Study_Forum/static_root'
+# STATIC_ROOT = '/home/Mrlearned/Study_Forum/static_root'
 
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
